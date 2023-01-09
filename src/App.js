e0815ecef33e44341ebd065b6bedcd44152157a9
fty57 <fty.57@alu.ufc.br>
2023-01-09 10:18:35 -0300

    useReducer - React Hooks - Parte 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import { createContext, useContext, useState } from 'react';
+import { useReducer } from 'react';
 import './App.css';
-import { Div } from './components/Div';
-import { AppContext } from './contexts/AppContext';
+
+const globalState = {
+  title: 'O título que contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda com', action.payload);
+      return { ...state, title: action.payload };
+    }
+
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+
+  console.log('Nenhuma action encontrada');
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'muda',
+            payload: new Date().toLocaleString('pt-BR'),
+          })
+        }
+      >
+        Click
+      </button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Click</button>
+      <button onClick={() => dispatch({ type: '' })}>Click</button>
+    </div>
   );
 }
 
