bb2084abd078c93a726b762442abad7bca61abd1
fty57 <fty.57@alu.ufc.br>
2023-01-10 11:17:39 -0300

    Organização com useEffect, useReducer, useContext e useRef - Parte 2

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,16 +1,19 @@
 import { Posts } from '../../components/Posts';
+import { CounterProvider } from '../../contexts/CounterProvider';
 import { PostsProvider } from '../../contexts/PostsProvider';
 import './styles.css';
 
 // App.jsx
 function App() {
   return (
-    <PostsProvider>
-      <div>
-        <h1>123</h1>
-        <Posts />
-      </div>
-    </PostsProvider>
+    <CounterProvider>
+      <PostsProvider>
+        <div>
+          <h1>123</h1>
+          <Posts />
+        </div>
+      </PostsProvider>
+    </CounterProvider>
   );
 }
 
