4be573e5845deaecd26e960518a2cc9e8ddfc7c2
fty57 <fty.57@alu.ufc.br>
2023-01-12 14:31:44 -0300

    Criando meu segundo hook - Parte 1

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './templates/App';
+import { Home } from './templates/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 );
---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = (url, options) => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const fetchData = async () => {
+      await new Promise((r) => setTimeout(r, 3000));
+      try {
+        const response = await fetch(url, options);
+        const jsonResult = await response.json();
+        setResult(jsonResult);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+        throw e;
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return [result, loading];
+};
+
+export const Home = () => {
+  const [result, loading] = useFetch(
+    'https://jsonplaceholder.typicode.com/posts',
+  );
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!loading && result) {
+    console.log(result);
+  }
+
+  return <h1>Home</h1>;
+};
