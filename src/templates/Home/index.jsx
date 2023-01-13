d972a5a719807b5c42b20829b44152e84fc6554b
fty57 <fty.57@alu.ufc.br>
2023-01-13 09:37:01 -0300

    62. Criando meu segundo hook - Parte 3

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,43 +1,110 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+const isObjectEqual = (objA, objB) => {
+  return JSON.stringify(objA) === JSON.stringify(objB);
+};
 
 const useFetch = (url, options) => {
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [shouldLoad, setShouldLoad] = useState(false);
+  const urlRef = useRef(url);
+  const optionsRef = useRef(options);
 
   useEffect(() => {
+    let changed = false;
+
+    if (!isObjectEqual(url, urlRef.current)) {
+      urlRef.current = url;
+      changed = true;
+    }
+
+    if (!isObjectEqual(options, optionsRef.current)) {
+      optionsRef.current = options;
+      changed = true;
+    }
+
+    if (changed) {
+      setShouldLoad((s) => !s);
+    }
+  }, [url, options]);
+
+  useEffect(() => {
+    let wait = false;
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     setLoading(true);
 
     const fetchData = async () => {
-      await new Promise((r) => setTimeout(r, 3000));
+      await new Promise((r) => setTimeout(r, 1000));
+
       try {
-        const response = await fetch(url, options);
+        const response = await fetch(urlRef.current, {
+          signal,
+          ...optionsRef.current,
+        });
         const jsonResult = await response.json();
-        setResult(jsonResult);
-        setLoading(false);
+
+        if (!wait) {
+          setResult(jsonResult);
+          setLoading(false);
+        }
       } catch (e) {
-        setLoading(false);
-        throw e;
+        if (!wait) {
+          setLoading(false);
+        }
+        console.log('MY ERROR: ' + e.message);
       }
     };
 
     fetchData();
-  }, [url]);
+
+    return () => {
+      wait = true;
+      controller.abort();
+    };
+  }, [shouldLoad]);
 
   return [result, loading];
 };
 
 export const Home = () => {
+  const [postId, setPostId] = useState('');
   const [result, loading] = useFetch(
-    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/posts/' + postId,
+    {
+      headers: {
+        abc: '1' + postId,
+      },
+    },
   );
 
   if (loading) {
     return <p>Loading...</p>;
   }
 
+  const handleClick = (id) => {
+    setPostId(id);
+  };
+
   if (!loading && result) {
-    console.log(result);
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+              <p>{p.title}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <p>{result.title}</p>
+          </div>
+        )}
+      </div>
+    );
   }
 
-  return <h1>Home</h1>;
+  return <h1>Oi</h1>;
 };
