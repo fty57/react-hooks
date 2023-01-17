7b21051ccb71fe719ffb97ad276b256fc76bedee
Álisson Véras <59287050+fty57@users.noreply.github.com>
2023-01-17 11:42:28 -0300

    73. Context API + Hooks - Parte 1

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './templates/App';
-import { Home } from './templates/Home/index5';
+import { Home } from './templates/Home/index6';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
