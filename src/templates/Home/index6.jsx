8fd20b3c5d04ba8cd0e96f86122ecc4e357dceec
Álisson Véras <59287050+fty57@users.noreply.github.com>
2023-01-19 11:38:21 -0300

    78. Context API + Hooks - Parte 6

--- src/templates/Home/index6.jsx
+++ src/templates/Home/index6.jsx
@@ -6,15 +6,33 @@ import { useCounterContext } from '../../contexts/CounterContext';
 export const Home = () => {
   const [state, actions] = useCounterContext();
 
-  useEffect(() => {
-    actions.increase();
-  }, [actions]);
+  const handleError = () => {
+    actions
+      .asyncError()
+      .then((r) =>
+        console.log(r).catch((e) => console.log(e.name, ':', e.message)),
+      );
+  };
 
   return (
     <div>
-      <Heading>{state.counter}</Heading>
+      <Heading />
       <div>
         <Button onButtonClick={actions.increase}>Increase</Button>
+        <Button onButtonClick={actions.decrease}>decrease</Button>
+        <Button onButtonClick={actions.reset}>reset</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 10 })}>
+          setCounter - 10
+        </Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 100 })}>
+          setCounter - 100
+        </Button>
+        <Button disabled={state.loading} onButtonClick={actions.asyncIncrease}>
+          asyncIncrease
+        </Button>
+        <Button disabled={state.loading} onButtonClick={handleError}>
+          asyncError
+        </Button>
       </div>
     </div>
   );
